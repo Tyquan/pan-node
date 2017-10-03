2d9e92a110046e372f2fa2f5f19b482400b1a093
Tyquan Reddick <Tjreddick@gmail.com>
2017-10-02 22:31:14 -0400

    updated

--- example/index.js
+++ example/index.js
@@ -2,28 +2,36 @@ var Pan = require("../index");
 
 var pa = new Pan();
 
-var myArray = [1,2,3,4,5,6,7];
-// mean average of the myArray population
-console.log(pa.mean(myArray));
+var arra = [1, 2, 3];
+var arra2 = [4, 5, 6];
+var arra3 = [];
 
-var myArray2 = [10,9,8,7,6,5,4,3,2,1];
-// median average of the myArray population
-pa.median(myArray2);
+console.log(pa.addV(arra3, arra, arra2));
 
-var myArray3 = [10,9,8,7,6,5,5,5,4,4,3,2,1];
-pa.mode(myArray3);
+// var pa = new Pan();
 
-var myArray4 = [0,0,5,5];
-pa.popVariance(myArray4);
+// var myArray = [1,2,3,4,5,6,7];
+// // mean average of the myArray population
+// console.log(pa.mean(myArray));
 
-var myArray5 = [1,2,3,8,7];
-pa.sampleVariance(myArray5);
+// var myArray2 = [10,9,8,7,6,5,4,3,2,1];
+// // median average of the myArray population
+// pa.median(myArray2);
 
-var myArray6 = [1,2,3,8,7];
-var population = pa.popVariance(myArray6);
-var sample = pa.sampleVariance(myArray6);
-pa.sDeviation(population);
-pa.sDeviation(sample);
+// var myArray3 = [10,9,8,7,6,5,5,5,4,4,3,2,1];
+// pa.mode(myArray3);
 
-var myArray7 = [1,2,3,8,7];
-console.log(pa.add(myArray7));
+// var myArray4 = [0,0,5,5];
+// pa.popVariance(myArray4);
+
+// var myArray5 = [1,2,3,8,7];
+// pa.sampleVariance(myArray5);
+
+// var myArray6 = [1,2,3,8,7];
+// var population = pa.popVariance(myArray6);
+// var sample = pa.sampleVariance(myArray6);
+// pa.sDeviation(population);
+// pa.sDeviation(sample);
+
+// var myArray7 = [1,2,3,8,7];
+// console.log(pa.add(myArray7));
--- index.js
+++ index.js
@@ -1,130 +1,100 @@
-var i;
-
-// Pan Object
-function Pan(){
-    this.mean = mean;
-    this.median = median;
-    this.mode = mode;
-    this.popVariance = popVariance;
-    this.sampleVariance = sampleVariance;
-    this.sDeviation = sDeviation;
-    this.add = add;
-    this.subtract = subtract;
-    this.multiply = multiply;
-    this.divide = divide;
-}
-
-// find the mean average of a population or sample
-function mean(array){
-    var sum = 0;
-    sum = this.add(array);
-    sum = this.divide(sum, array.length);
-    return sum;
-}
-
-// find the median average of a population or sample
-function median(array){
-    var sortedArray = array.sort();
-    if (sortedArray.length % 2 == 0) {
-        return 'sorted array is even ' + array.length;
-    } else {
-        var middle = Math.round(sortedArray.length / 2);
-        return middle;
-    }
-}
-
-// find the mode of a population or sample
-function mode(array){
-    var count = 1;
-    var temp = 0;
-    var tempCount;
-    var popular = array[0];
-    for (var i = 0; i < array.length - 1; i++){
-        temp = array[i];
-        tempCount = 0;
-        for (var j = 1; j < array.length; j++) {
-            if (temp == array[j]) {
-                tempCount++;
+class Pan {
+    mean(a){
+        if (a.length == 0){
+            return 0;
+        } 
+        else if(a.length == 1) {
+            return a;
+        }else {
+            var sum = 0;
+            for (var i = 0; i < a.length; i++) {
+                sum += a[i];
             }
-            if (tempCount > count) {
-                popular = temp;
-                count = tempCount;
-            }
+            sum = sum / a.length;
+            return sum;
         }
     }
-    return popular;
-}
 
-// find the variance of a population
-function popVariance(array){
-    var temp = [];
-    var reason = 0;
-    var result;
-    var mew = this.mean(array);
-    for (i = 0; i < array.length; i++) {
-        temp.push(Math.pow(array[i] - mew, 2));
+    // find the median average of a population or sample
+    median(array){
+        var sortedArray = array.sort();
+        if (sortedArray.length % 2 == 0) {
+            return 'sorted array is even ' + array.length;
+        } else {
+            var middle = Math.round(sortedArray.length / 2);
+            return middle;
+        }
     }
-    for (var j = 0; j < temp.length; j++) {
-        reason += temp[j];
+
+    // mode of a population or sample
+    mode(array){
+        var count = 1;
+        var temp = 0;
+        var tempCount;
+        var popular = array[0];
+        for (var i = 0; i < array.length - 1; i++){
+            temp = array[i];
+            tempCount = 0;
+            for (var j = 1; j < array.length; j++) {
+                if (temp == array[j]) {
+                    tempCount++;
+                }
+                if (tempCount > count) {
+                    popular = temp;
+                    count = tempCount;
+                }
+            }
+        }
+        return popular;
     }
-    result = reason / array.length;
-    return result;
-}
 
-// find the variance of a sample
-function sampleVariance(array){
-    var temp = [];
-    var reason = 0;
-    var result;
-    var mew = this.mean(array);
-    for (i = 0; i < array.length; i++) {
-        temp.push(Math.pow(array[i] - mew, 2));
+    // variance of a population
+    popVariance(array){
+        var temp = [];
+        var reason = 0;
+        var result;
+        var mew = this.mean(array);
+        for (var i = 0; i < array.length; i++) {
+            temp.push(Math.pow(array[i] - mew, 2));
+        }
+        for (var j = 0; j < temp.length; j++) {
+            reason += temp[j];
+        }
+        result = reason / array.length;
+        return result;
     }
-    for (var j = 0; j < temp.length; j++) {
-        reason += temp[j];
+
+    // find the variance of a sample
+    sampleVariance(array){
+        var temp = [];
+        var reason = 0;
+        var result;
+        var mew = this.mean(array);
+        for (var i = 0; i < array.length; i++) {
+            temp.push(Math.pow(array[i] - mew, 2));
+        }
+        for (var j = 0; j < temp.length; j++) {
+            reason += temp[j];
+        }
+        result = reason / array.length - 1;
+        return result;
     }
-    result = reason / array.length - 1;
-    return result;
-}
 
-// find the standard deviation of a sample or population
-// input must be a popVariance or sampleVariance
-function sDeviation(input){
-    var result = Math.sqrt(input);
-    return result;
-}
-
-// runs through array and adds its elements
-function add(array){
-    count = 0;
-    for (i = 0; i < array.length; i++) {
-        count+= array[i];
+    // standard deviation of a sample or population
+    // input must be a popVariance or sampleVariance
+    sDeviation(input){
+        var result = Math.sqrt(input);
+        return result;
     }
-    return count;
 }
 
-function subtract(a, b){
-    return a - b;
-}
+// module.exports = Pan;
 
-function divide(a, b){
-    return a / b;
-}
+var list = [1,2,3,4,5,1,6];
 
-function multiply(a, b){
-    return a * b;
-}
+var pan = new Pan();
+var total = pan.sampleVariance(list);
+var sdev = pan.sDeviation(total);
 
-module.exports = Pan;
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(total);
+console.log(sdev);
